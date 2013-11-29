aedae9f8d0da97cc7ec0b6d0bf23fb1f3f9b4d8b
Christopher Petersen <chris@petersen.io>
2013-11-29 13:55:32 -0800

    Trying another download url

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class postgres_app {
   package { 'postgres_app':
     provider => 'compressed_app',
-    source   => 'https://s3.amazonaws.com/github-cloud/releases/3946572/0eab3ca0-3fe0-11e3-9ec0-750a17741a74.zip?response-content-disposition=attachment%3B%20filename%3DPostgres93.zip&AWSAccessKeyId=AKIAISTNZFOVBIJMK3TQ&Expires=1385586386&Signature=0gBhqb93DVEpeXdud5VLWSBwjpA%3D'
+    source   => 'http://postgresapp.com/download'
   }
 }
